32c69a0d88e9197b5fde73c6c8aa5459d37da970
Hyrum Paramore <par17057@byui.edu>
2024-02-22 14:27:22 -0700

    configured some stuff for render

--- utilities/auth.js
+++ utilities/auth.js
@@ -7,7 +7,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/google/callback",
+    callbackURL: "https://cse341-project2-qlp6.onrender.com/google/callback",
     passReqToCallback: true
   },
   function(request, accessToken, refreshToken, profile, done) {

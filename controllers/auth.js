cac96aeb38451b5673c1e95824f9fb6eafba81e2
Hyrum Paramore <par17057@byui.edu>
2024-03-01 11:11:06 -0700

    commented out unused auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,38 +1,38 @@
 
 
-const login = async (req, res) => {
-  res.send(`<a href="/auth/google"> Authentication with Google</a>`);
-}
+// const login = async (req, res) => {
+//   res.send(`<a href="/auth/google"> Authentication with Google</a>`);
+// }
 
-const callback = async (req, res) => {
-  res.send(`Hello ${req.user.displayName}!`);
-}
+// const callback = async (req, res) => {
+//   res.send(`Hello ${req.user.displayName}!`);
+// }
 
-const protectedRoute = async (req, res) => {
-  res.send(`Hello ${req.user.displayName}!`);
-}
+// const protectedRoute = async (req, res) => {
+//   res.send(`Hello ${req.user.displayName}!`);
+// }
 
-const failure = async (req, res) => {
-  res.send('Failed to authenticate..');
-}
+// const failure = async (req, res) => {
+//   res.send('Failed to authenticate..');
+// }
 
-const logout = async (req, res) => {
-  req.logout(() => {
-    req.session.destroy((err) => {
-      if (err) {
-        console.log('Error destroying session:', err);
-        return res.sendStatus(500);
-      }
-      res.clearCookie('connect.sid'); // Clear the session cookie
-      res.send('Logged out successfully');
-    });
-  });
-}
+// const logout = async (req, res) => {
+//   req.logout(() => {
+//     req.session.destroy((err) => {
+//       if (err) {
+//         console.log('Error destroying session:', err);
+//         return res.sendStatus(500);
+//       }
+//       res.clearCookie('connect.sid'); // Clear the session cookie
+//       res.send('Logged out successfully');
+//     });
+//   });
+// }
 
-module.exports = {
-  login,
-  callback,
-  protectedRoute,
-  failure,
-  logout
-};
+// module.exports = {
+//   login,
+//   callback,
+//   protectedRoute,
+//   failure,
+//   logout
+// };
